38e96146d2901136ca397d52c0e89435ca25c422
Insane <luojieyy@gmail.com>
2013-08-18 02:25:16 +0800

    shit

--- file.php
+++ file.php
@@ -2,10 +2,16 @@
 
 	// https://code.google.com/p/php-excel-reader/wiki/Documentation
 	// http://eden.openovate.com/documentation/start
-
+	ini_set('default_charset', 'utf-8'); 
+	
 	require('spreadsheet-reader/php-excel-reader/excel_reader2.php');
 	require('spreadsheet-reader/SpreadsheetReader.php');
 	include('eden.php');
+
+	include('pdo/config.php');
+	include('pdo/Mysql.php');
+	include('pdo/class.lpdo.php');
+
 
 	$action = $_GET['action'];
 	if ($action && function_exists($action)) {
@@ -14,25 +20,45 @@
 		echo json_encode(array('msg'=>'woops!'));
 	}
 
+	function array_iconv($in_charset,$out_charset,$arr){    
+    	return eval('return '.iconv($in_charset,$out_charset,var_export($arr,true).';'));    
+	}  
+
 	/*
 		初始化数据
 	*/
 
 	function init(){
-		// $data = new Spreadsheet_Excel_Reader("songs.xls",false);
-		// echo $data->rowcount();
+		$data = new Spreadsheet_Excel_Reader("songs.xls",false);
 
-		$limit = 500;
+		$limit = 2;
 		$page = 0;
-		$rows = array();
+		$i = 2;
+		
+		$db = new Mysql('songs','root','root','192.168.1.104');
 
-		while ($i < $limit*($page+1)) {
+		$insert = array();
+		$sql = 'INSERT INTO songs (filename,song,language,all_singer,singer_1,singer_2,original,version) VALUES ';
+		while($i<=10) {
+
+
+			$sql.="('".$data->val($i,1)."','";
+			$sql.= $data->val($i,2)."','";
+			$sql.= $data->val($i,3)."','";
+			$sql.= $data->val($i,4)."','";
+			$sql.= $data->val($i,5)."','";
+			$sql.= $data->val($i,6)."','";
+			$sql.= $data->val($i,13)."','";
+			$sql.= $data->val($i,15)."')";
+			if ($i !=10) {
+				$sql.=",";
+			}
 			
+			$i++;
 		}
-		$database = eden('mysql', '127.0.0.1' ,'songs', 'root', 'root');
-
-		// $excel = new SimpleExcel('CSV');
-		// $excel->parser->loadFile('songs.xls');
+		echo "<br>";
+		var_dump($db);
+		$db->ExecuteSQL($sql);
 	}
 
 	
---
+++ pdo/config.php
@@ -0,0 +1,11 @@
+<?php
+$config = array();
+$config['Database'] = array();
+$config['Database']['dbtype'] = 'mysqli';
+$config['Database']['dbname'] = 'songs';
+$config['Database']['host'] = '192.168.1.104';
+$config['Database']['port'] = 3306;
+$config['Database']['username'] = 'root';
+$config['Database']['password'] = 'root';
+$config['Database']['charset'] = 'utf8';
+?>
