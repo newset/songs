fe023197007e83e7159c4616606c3e53a49e24a7
Insane <luojieyy@gmail.com>
2013-08-17 23:53:02 +0800

    refine router

--- file.php
+++ file.php
@@ -1,10 +1,18 @@
 <?php 
+
+	// https://code.google.com/p/php-excel-reader/wiki/Documentation
+	// http://eden.openovate.com/documentation/start
+
 	require('spreadsheet-reader/php-excel-reader/excel_reader2.php');
 	require('spreadsheet-reader/SpreadsheetReader.php');
 	include('eden.php');
 
 	$action = $_GET['action'];
-	call_user_func($action);
+	if ($action && function_exists($action)) {
+		call_user_func($action);
+	}else{
+		echo json_encode(array('msg'=>'woops!'));
+	}
 
 	/*
 		初始化数据
@@ -15,8 +23,12 @@
 		// echo $data->rowcount();
 
 		$limit = 500;
-		$page = 1;
+		$page = 0;
+		$rows = array();
 
+		while ($i < $limit*($page+1)) {
+			
+		}
 		$database = eden('mysql', '127.0.0.1' ,'songs', 'root', 'root');
 
 		// $excel = new SimpleExcel('CSV');
